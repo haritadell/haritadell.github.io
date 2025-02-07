1955f96050e2fd10ff5bc4264cb1ec5be0f83753
haritadell <h.dellaporta@gmail.com>
2025-02-07 23:04:52 +0000

    Deploying to gh-pages from @ haritadell/haritadell.github.io@50fa4a0df33687df4377bd79eedf9ce7d307c214 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-invited-presentations",
-          title: "invited presentations",
+        },{id: "nav-presentations",
+          title: "presentations",
           description: "",
           section: "Navigation",
           handler: () => {
